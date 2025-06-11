39bce8605f6b2e06b3b136ed688eed9cbf544934
INGRoyer <stephroyer@users.noreply.github.com>
2025-06-10 21:27:26 -0500

    update section7

--- store/order.js
+++ store/order.js
@@ -1,4 +1,4 @@
 export default {
-  header: "My Home Page", // The text that shows on the view
-  view: "home" // This should match the view file name
+  header: "order a pizza", // The text that shows on the view
+  view: "order" // This should match the view file name
 };
